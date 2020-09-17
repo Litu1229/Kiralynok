79589b4e2917830302fcf822ef3ce76de08a34c9
Ripperoni <lcsabi9d@gmail.com>
2020-09-17 08:05:07 +0200

    Add project files.

---
+++ Kiralynok/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiralynok
+{
+    class Tabla
+    {
+        private char[,] T;
+        private char Urescella;
+        private int UresOszlopokSzama;
+        private int UresSorokSzama;
+        public Tabla(char ch)
+        {
+            T = new char[8, 8];
+            Urescella = ch;
+            int s = 8;
+            int o = 8;
+            for (int i = 0; i < s; i++)
+            {
+                for (int j = 0; j < o; j++)
+                {
+                    T[i, j] = Urescella;
+                }
+            }
+        }
+        public void Elhelyez(int N)
+        {
+            //1. Véletlen helyérték létrehozása
+            //  - Random osztály értékek halmaza:[0,7]
+            //  - Véletlen sor,oszlop kell
+            //  - elhelyezzük a "K"-t csak akkor
+            //              HA!!!!! üres -> "#"
+
+            Random vel = new Random();
+            int sor = vel.Next(0, 8);
+            int oszlop = vel.Next(0, 8);
+            if (T[sor, oszlop] == '#')
+            {
+                T[sor, oszlop] = 'K';
+            }
+        }
+        public void FajlbaIr()
+        {
+
+        }
+        public void Megjelenit()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < 8; j++)
+                {
+                    Console.Write("{0,-2}", T[i, j]);
+                }
+            }
+        }
+        public int UresOszlop()
+        {
+            return 0;
+        }
+        public int UresSor()
+        {
+            return 0;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Királynők feladat:");
+            Console.WriteLine();
+            Tabla t = new Tabla('#');
+            Console.WriteLine("Üres tábla: ");
+            t.Megjelenit();
+            Console.WriteLine();
+            t.Elhelyez(1);
+            Console.WriteLine();
+            Console.WriteLine("Királynő elhelyezése:");
+            t.Megjelenit();
+            //N darab királynő elhelyezése
+            Console.ReadKey();
+        }
+    }
+}
+
